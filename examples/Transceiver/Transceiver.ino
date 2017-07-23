17ac8d683b1c4b9c9078488a6f808d90674e12e4
Vittorio Esposito <esposito.vittorio@rocketmail.com>
2017-07-23 15:52:45 +0200

    Add files via upload

--- examples/Transceiver/Transceiver.ino
+++ examples/Transceiver/Transceiver.ino
@@ -4,38 +4,28 @@
 
   J1850-Arduino-Transceiver-Library
 */
+#define EI_ARDUINO_INTERRUPTED_PIN
+#include <EnableInterrupt.h>
 #include <PwmTransceiver.h>
 
-volatile bool PwmTransceiver::flagDecoder;
-volatile unsigned long PwmTransceiver::timeOld;
 const int rx_pin = 3;   // https://goo.gl/nHC95v
 const int tx_pin = 13;
 
 PwmTransceiver Transceiver(rx_pin, tx_pin);
 
-void setup() 
-{
+void setup() {
   Serial.begin(2000000);
   Serial.println("PwmTransceiver");
   Transceiver.begin(1200);
 }
 
-void loop() 
-{
-  while (1)
-  {
-    while (Transceiver.isReceiving())
-    {
-      Transceiver.receive();
-    }
-    if (Transceiver.available())
-    {
-      Serial.print(Transceiver.readString());
-    }
-    if (Serial.available()) 
-    {
-      Transceiver.print(Serial.readString());
-    }
-    // Put non-blocking code here
+void loop() {
+  while (Transceiver.isReceiving());
+  if (Transceiver.available()) {
+    Serial.print(Transceiver.readString());
   }
+  if (Serial.available()) {
+    Transceiver.print(Serial.readString());
+  }
+  // Put non-blocking code here
 }
--- examples/Transmit_and_power_down/Transmit_and_power_down.ino
+++ examples/Transmit_and_power_down/Transmit_and_power_down.ino
@@ -4,10 +4,10 @@
 
   J1850-Arduino-Transceiver-Library
 */
+#define EI_ARDUINO_INTERRUPTED_PIN
+#include <EnableInterrupt.h>
 #include <PwmTransceiver.h>
 
-volatile bool PwmTransceiver::flagDecoder;
-volatile unsigned long PwmTransceiver::timeOld;
 const int rx_pin = 3;   // https://goo.gl/nHC95v
 const int tx_pin = 13;
 const int en_pin = 11;  // To power up/down the module
@@ -17,7 +17,8 @@ PwmTransceiver Transceiver(rx_pin, tx_pin);
 String str;
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(2000000);
+  Serial.println("PwmTransceiver");
   Transceiver.begin(1200);              // 1200 bit per second
   Transceiver.setEnablePin(en_pin);     // Set power up/down pin
   Transceiver.powerDown();              // Power down module
@@ -25,14 +26,14 @@ void setup() {
 
 void loop() {
 
-  // Alternative way: send a string
+  // Send a string
   if (Serial.available() > 0) {
     Transceiver.powerUp();            	// Power up module
     delay(1000);                        // Radio-module is powering up
     str = Serial.readString();          // One second timeOut
     Transceiver.print(str);
     Serial.println(str);
-    delay(1000);                        
+    delay(1000);
     Transceiver.powerDown();          	// Power down module
   }
 
